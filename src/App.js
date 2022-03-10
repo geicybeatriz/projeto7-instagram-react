9c4d29167649cbd4dcbd16caf11cb3a1640f2d75
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-03-10 15:41:24 -0300

    Componentizaçao iniciada

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Container from './Container';
+import Rodape from './Rodape';
+
+export default function App (){
+    return (
+        <div>
+            <Navbar/>
+            <Container/>
+            <Rodape/>
+        </div>
+    );
+}
